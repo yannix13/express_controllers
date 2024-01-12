bca487b7a343560c1d3763e0e601821c073c5c75
Yannis S <58774206+yannix13@users.noreply.github.com>
2024-01-12 14:57:42 +0100

    add update movies route

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const movieControllers = require("./src/controllers/movieControllers");
 const userControllers = require("./src/controllers/userControllers");
 
+// Movies routes
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
+app.post("/api/movies", movieControllers.postMovie);
+
+app.put("/api/movies/:id", movieControllers.updateMovie)
+
+
+// Users routes
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
+
+
+
 
 module.exports = app;
 
